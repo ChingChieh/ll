0581820e7dde233195e28f7cb9675d7b865f4513
ChingChieh <garnett.huang@gmail.com>
2019-01-12 00:21:00 +0800

    Have some bugs

--- test_remove.c
+++ test_remove.c
@@ -4,14 +4,18 @@
 #include <stdint.h>
 #include "ll.h"
 
-#define DEFAULT_THREAD_NUM 1 
+#define DEFAULT_THREAD_NUM 1
+#define DEFAULT_NODE_NUM 20
 ll_t *list;
 void *test(void *ptr) {
-    ll_insert_first(list, &((uint32_t*)ptr)[3]); 
-	ll_insert_first(list, &((uint32_t*)ptr)[2]);
-	ll_insert_first(list, &((uint32_t*)ptr)[1]);
-	ll_insert_first(list, &((uint32_t*)ptr)[4]);
-	ll_insert_first(list, &((uint32_t*)ptr)[0]);
+	if(*(int*)ptr > DEFAULT_NODE_NUM){
+		return NULL;
+	}
+	int it = DEFAULT_NODE_NUM / *(int*)ptr;
+	for (int i = 0; i < it; ++i) {
+		ll_print(*list);
+		ll_remove_n(list,(list->len % it));
+	}
 	return NULL;
 }
 
@@ -23,6 +27,7 @@ int main(int argc, char** argv){
 	// Initialize global list 
     list = ll_new(num_teardown);
     list->val_printer = num_printer;
+	uint32_t node_num = DEFAULT_NODE_NUM;
     // Start to create threads 
     pthread_t *threads;
     if ((threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t))) == NULL){
@@ -31,10 +36,13 @@ int main(int argc, char** argv){
     }
 	// Testing array & empty array use for checking correctness
 	uint32_t num_array[5] = {0,1,2,3,4};
-	uint32_t check[5] = {};
+	// Create a 10000 nodes list
+	for (int i = 0; i < node_num; ++i) {
+		ll_insert_last(list,&num_array[i % 5]);	
+	}
 
     for (int i = 0; i < num_threads; ++i) {
-		if(pthread_create(&threads[i], NULL, test, (void*)num_array) != 0){
+		if(pthread_create(&threads[i], NULL, test, &num_threads) != 0){
             fprintf(stderr, "Error creating thread\n");
             exit(1);
         }
@@ -48,19 +56,6 @@ int main(int argc, char** argv){
     }
    	 
 	ll_print(*list);
-	ll_node_t *it = list->hd;
-	for (int i = 0; i < list->len; ++i) {
-		check[*(uint32_t*)(it->val)]++;
-		it = it->nxt;
-	}
-
-	printf("# Expect list len: %d got %d\n",num_threads * 5, list->len);
-	printf("# Expect 0's amount: %d got %d\n",num_threads,check[0]);
-	printf("# Expect 1's amount: %d got %d\n",num_threads,check[1]);
-	printf("# Expect 2's amount: %d got %d\n",num_threads,check[2]);
-	printf("# Expect 3's amount: %d got %d\n",num_threads,check[3]);
-	printf("# Expect 4's amount: %d got %d\n",num_threads,check[4]);
-
     free(threads);
     ll_delete(list);
     return 0;
